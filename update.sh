ef3f0d029a7e8674750701be126a7755005585be
afedotov <afedotov@team.amocrm.com>
2021-07-18 17:30:32 +0300

    init

---
+++ update.sh
@@ -0,0 +1,7 @@
+# Конфиг vscode 
+rm -rf configs/settings.json
+cp ~/Library/Application\ Support/Code/User/settings.json ./configs
+
+# Горячие клавиши
+rm -rf configs/keybindings.json
+cp ~/Library/Application\ Support/Code/User/keybindings.json ./configs
